60eb9c22eaf12e2781d58dd4b9f8b7e5fd4317d5
Murali Annamneni <murali.annamneni@oracle.com>
2017-10-17 14:15:12 +0100

    Enable MySQL Cluster Support for fortinet
    This enables the usage of the mysql_enable_ndb setting from oslo.db
    from bug 1564110.This allows operators to select from MySQL (InnoDB)
    or MySQL Cluster (NDB) as the storage engine backend.
    
    Depends-On: I9f1fd2a87fdf75332de2339d3ff4f08ce9220dcf
    Change-Id: Iec5f7ab7738f2c9728af90dcd74ab3e2d3f1a28d
    Implements: blueprint mysql-cluster-support

--- networking_fortinet/db/migration/alembic_migrations/env.py
+++ networking_fortinet/db/migration/alembic_migrations/env.py
@@ -32,7 +32,10 @@ target_metadata = model_base.BASEV2.metadata
 
 def set_mysql_engine():
     try:
-        mysql_engine = neutron_config.command.mysql_engine
+        if neutron_config.database.mysql_enable_ndb:
+            mysql_engine = 'NDBCLUSTER'
+        else:
+            mysql_engine = neutron_config.command.mysql_engine
     except cfg.NoSuchOptError:
         mysql_engine = None
 
@@ -64,20 +67,23 @@ def set_storage_engine(target, parent):
 
 def run_migrations_online():
     set_mysql_engine()
-    engine = session.create_engine(neutron_config.database.connection)
+    engine = session.create_engine(
+              sql_connection=neutron_config.database.connection,
+              mysql_enable_ndb=neutron_config.database.mysql_enable_ndb
+             )
 
-    connection = engine.connect()
-    context.configure(
-        connection=connection,
-        target_metadata=target_metadata,
-        version_table=FGT_VERSION_TABLE
-    )
-    try:
-        with context.begin_transaction():
-            context.run_migrations()
-    finally:
-        connection.close()
-        engine.dispose()
+    with engine.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            version_table=FGT_VERSION_TABLE
+        )
+        try:
+            with context.begin_transaction():
+                context.run_migrations()
+        finally:
+            connection.close()
+            engine.dispose()
 
 
 if context.is_offline_mode():
